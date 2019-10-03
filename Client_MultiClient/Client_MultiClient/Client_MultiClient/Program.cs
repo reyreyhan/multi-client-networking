05b9afb2f4d099e25e36dd05a86b90d927c88528
Reyhan Alphard Savero <newrey9227@gmail.com>
2019-10-03 15:27:30 +0700

    First Commit

---
+++ Client_MultiClient/Client_MultiClient/Client_MultiClient/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+public class Client
+{
+    static public void Main(string[] Args)
+    {
+        TcpClient socketForServer;
+        try
+        {
+            socketForServer = new TcpClient("192.168.1.102", 9999);
+        }
+        catch
+        {
+            Console.WriteLine(
+            "Failed to connect to server at {0}:999", "localhost");
+            return;
+        }
+
+        NetworkStream networkStream = socketForServer.GetStream();
+        System.IO.StreamReader streamReader =
+        new System.IO.StreamReader(networkStream);
+        System.IO.StreamWriter streamWriter =
+        new System.IO.StreamWriter(networkStream);
+
+        try
+        {
+            string outputString;
+            {
+                Console.WriteLine("Tulis Pesan :");
+                string str = Console.ReadLine();
+                while (str != "exit")
+                {
+                    streamWriter.WriteLine(str);
+                    streamWriter.Flush();
+                    Console.WriteLine("Tulis Pesan :");
+                    str = Console.ReadLine();
+                }
+                if (str == "exit")
+                {
+                    streamWriter.WriteLine(str);
+                    streamWriter.Flush();
+
+                }
+
+            }
+        }
+        catch
+        {
+            Console.WriteLine("Exception reading from Server");
+        }
+
+        networkStream.Close();
+        Console.WriteLine("Press any key to exit from client program");
+        Console.ReadKey();
+    }
+
+    private static string GetData()
+    {
+        return "ack";
+    }
+}
---
+++ Server_MultiClient/Server_MultiClient/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Threading;
+public class AsynchIOServer
+{
+    static TcpListener tcpListener = new TcpListener(9999);
+    public static string kata;
+    static void Listeners()
+    {
+
+        Socket socketForClient = tcpListener.AcceptSocket();
+        if (socketForClient.Connected)
+        {
+            Console.WriteLine("Client:" + socketForClient.RemoteEndPoint + " now connected to server.");
+            NetworkStream networkStream = new NetworkStream(socketForClient);
+            System.IO.StreamWriter streamWriter =
+            new System.IO.StreamWriter(networkStream);
+            System.IO.StreamReader streamReader =
+            new System.IO.StreamReader(networkStream);
+
+            while (true)
+            {
+                string theString = streamReader.ReadLine();
+                Console.WriteLine("Message recieved by client:" + theString);
+                if (theString == "exit")
+                    break;
+                if (theString == kata)
+                    Console.WriteLine("Menang");
+            }
+            streamReader.Close();
+            networkStream.Close();
+            streamWriter.Close();
+            
+
+        }
+
+        socketForClient.Close();
+        Console.WriteLine("Press any key to exit from server program");
+        Console.ReadKey();
+    }
+
+    public static void Main()
+    {
+        Console.WriteLine("Server Mamasukkan Kata : ");
+        kata = Console.ReadLine();
+
+        tcpListener.Start();
+        Console.WriteLine("Client Waiting");
+        
+        int numberOfClientsYouNeedToConnect = 3;
+
+        for (int i = 0; i < numberOfClientsYouNeedToConnect; i++)
+        {
+            Thread newThread = new Thread(new ThreadStart(Listeners));
+            newThread.Start();
+        }
+        
+    }
+}
